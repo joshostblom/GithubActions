ceb477c362b32730b5158e26044add565c4ef184
joshostblom <josh.ostblom@gmail.com>
2023-11-01 17:13:11 -0500

    Fix test.

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -81,25 +81,25 @@ namespace GithubActionsLab
         [Test]
         public void Divide_Valid_Ostblom()
         {
-            Assert.AreEqual(5, Program.Multiply("10", "2"));
-            Assert.AreEqual(14, Program.Multiply("28", "2"));
-            Assert.AreEqual(35, Program.Multiply("70", "2"));
+            Assert.AreEqual(5, Program.Divide("10", "2"));
+            Assert.AreEqual(14, Program.Divide("28", "2"));
+            Assert.AreEqual(35, Program.Divide("70", "2"));
         }
 
         [Test]
         public void Divide_Invalid_Ostblom()
         {
-            Assert.Throws<FormatException>(() => Program.Multiply("1", "a"));
-            Assert.Throws<FormatException>(() => Program.Multiply("a", "1"));
-            Assert.Throws<FormatException>(() => Program.Multiply("a", "a"));
+            Assert.Throws<FormatException>(() => Program.Divide("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "a"));
         }
 
         [Test]
         public void Divide_Null_Ostblom()
         {
-            Assert.Throws<ArgumentNullException>(() => Program.Multiply("1", null));
-            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, "1"));
-            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, null));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, null));
         }
     }
 }
